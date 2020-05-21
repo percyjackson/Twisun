28aa34c58c5e22a716da858b44c4c24c87fd9bf0
Yeison Osorio Jaramillo <yosorio3@eafit.edu.co>
2020-05-21 14:07:39 -0500

    Data context and String connection to DB

---
+++ Twisun.Web/Data/DataContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Twisun.Web.Data.Entities;
+
+namespace Twisun.Web.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<Battery> Batteries { get; set; }
+        public DbSet<DailySolarEnergy> DailySolarEnergies { get; set; }
+        public DbSet<Range> Ranges { get; set; }
+        public DbSet<SolarPanel> SolarPanels { get; set; }
+    }
+}
