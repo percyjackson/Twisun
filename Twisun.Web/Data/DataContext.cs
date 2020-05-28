ea77f09cdb207f62b9ebaa688e32537b79e3d186
Yeison Osorio Jaramillo <yosorio3@eafit.edu.co>
2020-05-27 20:09:12 -0500

    Adding entities on DbContext based on net core user identity

--- Twisun.Web/Data/DataContext.cs
+++ Twisun.Web/Data/DataContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,16 +8,22 @@ using Twisun.Web.Data.Entities;
 
 namespace Twisun.Web.Data
 {
-    public class DataContext : DbContext
+    public class DataContext : IdentityDbContext<User>
     {
         public DataContext(DbContextOptions<DataContext> options) : base(options)
         {
         }
 
+        public DbSet<Owner> Owners { get; set; }
         public DbSet<Car> Cars { get; set; }
         public DbSet<Battery> Batteries { get; set; }
         public DbSet<DailySolarEnergy> DailySolarEnergies { get; set; }
         public DbSet<Range> Ranges { get; set; }
         public DbSet<SolarPanel> SolarPanels { get; set; }
+        public DbSet<Partner> Partners { get; set; }
+        public DbSet<Coupon> Coupons { get; set; }
+        public DbSet<UsedCoupon> UsedCoupons { get; set; }
+        public DbSet<Point> Points { get; set; }
+        public DbSet<Manager> Managers { get; set; }
     }
 }
